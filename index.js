ab6bfdd55c0d8cd746e8fe278b2a800f102e9eb2
spenser6131 <brinkman.spenser@gmail.com>
2021-01-20 19:10:02 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+  
+  static titleize(string) {
+    let s = [];
+    let rejects = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = string.split(" ");
+    let l = words.length;
+    for (let i = 0; i < l; i++) {
+      if (i === 0) {
+        s.push(this.capitalize(words[i]))
+      } else {
+        if (rejects.includes(words[i])) {
+          s.push(words[i]);
+        } else {
+          s.push(this.capitalize(words[i]));
+        }
+      }
+    }
+    return s.join(" ")
+  }
 }
+
